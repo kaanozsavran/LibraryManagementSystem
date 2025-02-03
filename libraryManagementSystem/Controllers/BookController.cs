126ab82444eb3086dbea1cfae92e26990e5a7b82
kaanozsavran <kaanozsavran@hotmail.com>
2025-02-03 17:46:25 +0300

    applied HttpGet and HttpPost methods

--- libraryManagementSystem/Controllers/BookController.cs
+++ libraryManagementSystem/Controllers/BookController.cs
@@ -1,12 +1,29 @@
+using System.Threading.Tasks;
+using libraryManagementSystem.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace libraryManagementSystem.Controllers {
 
     public class BookController : Controller{
          
+        private readonly DataContext _context;
+        public BookController(DataContext context){
+            _context = context;
+        }
 
+   
+
+
+        [HttpGet]
         public IActionResult Create(){
             return View();
+        }
+
+        [HttpPost]
+         public async Task<IActionResult> Create(Book model){
+            _context.Books.Add(model);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
 
     }
