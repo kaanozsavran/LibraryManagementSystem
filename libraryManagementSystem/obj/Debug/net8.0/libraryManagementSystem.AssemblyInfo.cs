ce3f00f4b53883c78cfa84e1cf9ed770268d7014
kaanozsavran <kaanozsavran@hotmail.com>
2025-02-04 23:33:09 +0300

    added layoutnofooter, updated index,create,edit.cshtml pages

--- libraryManagementSystem/obj/Debug/net8.0/libraryManagementSystem.AssemblyInfo.cs
+++ libraryManagementSystem/obj/Debug/net8.0/libraryManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("libraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+680b9acf0b898e029fb388b086e5b09921d9b419")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d41669134fd468c097fe7a5701594bff4761a92")]
 [assembly: System.Reflection.AssemblyProductAttribute("libraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("libraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
