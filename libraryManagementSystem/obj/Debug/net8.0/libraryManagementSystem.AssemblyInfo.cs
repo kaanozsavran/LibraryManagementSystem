a7573838abbd990e3082a94500707f6b3759989e
kaanozsavran <kaanozsavran@hotmail.com>
2025-02-04 15:34:28 +0300

    updated for select list

--- libraryManagementSystem/obj/Debug/net8.0/libraryManagementSystem.AssemblyInfo.cs
+++ libraryManagementSystem/obj/Debug/net8.0/libraryManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("libraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+126ab82444eb3086dbea1cfae92e26990e5a7b82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+758be191595781428ce210deaf94484ec7fc4c1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("libraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("libraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
